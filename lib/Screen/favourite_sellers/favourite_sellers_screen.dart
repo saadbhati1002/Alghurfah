ae38c355538394f78997871da0c5f56b21421d3c
saadbhati1002 <saadbhati1002@gmail.com>
2023-12-14 14:53:10 +0530

    app upload code

--- lib/Screen/favourite_sellers/favourite_sellers_screen.dart
+++ lib/Screen/favourite_sellers/favourite_sellers_screen.dart
@@ -3,7 +3,9 @@ import 'package:eshop_multivendor/Helper/String.dart';
 import 'package:eshop_multivendor/Helper/routes.dart';
 import 'package:eshop_multivendor/Model/favorite_seller_model.dart';
 import 'package:eshop_multivendor/Screen/Language/languageSettings.dart';
+import 'package:eshop_multivendor/ServiceApp/component/empty_error_state_widget.dart';
 import 'package:eshop_multivendor/ServiceApp/component/loader_widget.dart';
+import 'package:eshop_multivendor/main.dart';
 import 'package:eshop_multivendor/repository/sellerDetailRepositry.dart';
 import 'package:eshop_multivendor/widgets/background_image.dart';
 import 'package:eshop_multivendor/widgets/desing.dart';
@@ -55,113 +57,120 @@ class _FavoriteSellersScreenState extends State<FavoriteSellersScreen> {
       children: [
         const BackgroundImage(),
         isLoading == false
-            ? SizedBox(
-                child: SingleChildScrollView(
-                  child: SizedBox(
-                    // height: 100,
-                    width: MediaQuery.of(context).size.width,
-                    child: GridView.builder(
-                      padding: const EdgeInsets.all(20),
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              mainAxisSpacing: 8,
-                              childAspectRatio: .68,
-                              crossAxisSpacing: 8),
-                      shrinkWrap: true,
-                      itemCount: favoriteList.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ClipRRect(
-                                    borderRadius: const BorderRadius.only(
-                                        topRight: Radius.circular(25),
-                                        bottomLeft: Radius.circular(25)),
+            ? favoriteList.isNotEmpty
+                ? SizedBox(
+                    child: SingleChildScrollView(
+                      child: SizedBox(
+                        // height: 100,
+                        width: MediaQuery.of(context).size.width,
+                        child: GridView.builder(
+                          padding: const EdgeInsets.all(20),
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  mainAxisSpacing: 8,
+                                  childAspectRatio: .68,
+                                  crossAxisSpacing: 8),
+                          shrinkWrap: true,
+                          itemCount: favoriteList.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ClipRRect(
+                                        borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(25),
+                                            bottomLeft: Radius.circular(25)),
+                                        child: Container(
+                                          width:
+                                              MediaQuery.of(context).size.width,
+                                          decoration: BoxDecoration(
+                                            color: context.cardColor,
+                                          ),
+                                          child: DesignConfiguration
+                                              .getCacheNotworkImage(
+                                            boxFit: BoxFit.fill,
+                                            context: context,
+                                            heightvalue: null,
+                                            widthvalue: null,
+                                            placeHolderSize: 50,
+                                            imageurlString:
+                                                favoriteList[index].logo!,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    favoriteList[index].storeName!,
+                                    textAlign: TextAlign.center,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodySmall!
+                                        .copyWith(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'ubuntu',
+                                        ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 15),
                                     child: Container(
+                                      height: 28,
                                       width: MediaQuery.of(context).size.width,
-                                      decoration: BoxDecoration(
-                                        color: context.cardColor,
+                                      decoration: const BoxDecoration(
+                                        color: colors.serviceColor,
+                                        borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(20),
+                                            bottomLeft: Radius.circular(20)),
                                       ),
-                                      child: DesignConfiguration
-                                          .getCacheNotworkImage(
-                                        boxFit: BoxFit.fill,
-                                        context: context,
-                                        heightvalue: null,
-                                        widthvalue: null,
-                                        placeHolderSize: 50,
-                                        imageurlString:
-                                            favoriteList[index].logo!,
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        getTranslated(context, 'View')!,
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ),
-                              Text(
-                                favoriteList[index].storeName!,
-                                textAlign: TextAlign.center,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodySmall!
-                                    .copyWith(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'ubuntu',
-                                    ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 15),
-                                child: Container(
-                                  height: 28,
-                                  width: MediaQuery.of(context).size.width,
-                                  decoration: const BoxDecoration(
-                                    color: colors.serviceColor,
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(20),
-                                        bottomLeft: Radius.circular(20)),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    getTranslated(context, 'View')!,
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
+                                ],
                               ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                            ],
-                          ),
-                          onTap: () {
-                            Routes.navigateToSellerProfileScreen(
-                                context,
-                                favoriteList[index].userId!,
-                                favoriteList[index].logo!,
-                                favoriteList[index].storeName!,
-                                favoriteList[index].rating!,
-                                favoriteList[index].storeName!,
-                                favoriteList[index].storeDescription!,
-                                '0', []);
+                              onTap: () {
+                                Routes.navigateToSellerProfileScreen(
+                                    context,
+                                    favoriteList[index].userId!,
+                                    favoriteList[index].logo!,
+                                    favoriteList[index].storeName!,
+                                    favoriteList[index].rating!,
+                                    favoriteList[index].storeName!,
+                                    favoriteList[index].storeDescription!,
+                                    '0', []);
+                              },
+                            );
                           },
-                        );
-                      },
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              )
+                  )
+                : NoDataWidget(
+                    title: language.noProviderFound,
+                    subTitle: language.noProviderFoundMessage,
+                    imageWidget: const EmptyStateWidget(),
+                  )
             : Observer(
                 builder: (BuildContext context) =>
                     LoaderWidget().visible(isLoading)),
